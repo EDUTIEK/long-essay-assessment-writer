ee93a9d2ab5a34858467131c3899bb9e3794b314
fneumann <neumann@ilias.de>
2024-10-04 17:05:58 +0200

    Use PDF.js for instructions PDF

--- src/components/InstructionsPdf.vue
+++ src/components/InstructionsPdf.vue
@@ -7,33 +7,105 @@ const layoutStore = useLayoutStore();
 const resourcesStore = useResourcesStore();
 const resource = resourcesStore.getInstruction;
 
+let pdfApp = null;
+let pdfConstants = null;
+let pdfOptions = null;
+let pdfLib = null;
+let pdfDoc = null;
+
+async function loaded(event) {
+
+  const win = event.target.contentWindow;
+
+  pdfApp = win.PDFViewerApplication;
+  pdfConstants = win.PDFViewerApplicationConstants;
+  pdfOptions = win.PDFViewerApplicationOptions;
+  pdfLib = win.pdfjsLib;
+
+  // this works!
+  // win.addEventListener("keydown", logEvent)
+
+  console.log(pdfApp);
+
+
+  pdfApp.eventBus.on('pagesloaded', async function(event) {
+    pdfDoc = pdfApp.pdfDocument;
+    console.log(pdfDoc);
+  });
+
+
+  // seems not to be needed, objects are already there in load event
+  // https://medium.com/@chhavikhandelwal/ensuring-smooth-execution-how-to-wait-for-objects-on-window-load-in-javascript-a39fbd1d2e73
+  // const intervalId = setInterval(() => {
+  //   if (window.thatObject) {
+  //     clearInterval(intervalId);
+  //     resolve(window.thatObject);
+  //   }
+  // }, 100);
+}
+
+async function logEvent(event) {
+  console.log(event);
+}
+
+
 async function handleFocusChange() {
   if (layoutStore.focusTarget == 'left' && layoutStore.isInstructionsPdfVisible) {
     await nextTick();
     document.getElementById('app-instructions-pdf').focus();
   }
 }
-
 watch(() => layoutStore.focusChange, handleFocusChange);
+
+
+async function test() {
+  let num = 1;
+  while (num <= pdfDoc.numPages) {
+    const page = await pdfDoc.getPage(num);
+    const annotations = await page.getAnnotations();
+    console.log(annotations);
+    num++;
+  }
+}
+
+
 </script>
 
 <template>
-  <div id="app-instructions-pdf" tabindex="0">
-    <object
-        v-if="resource.mimetype =='application/pdf'"
-        type="application/pdf"
-        :data="resource.url"
-        width="100%"
-        height="100%">
-    </object>
+  <div id="app-instructions-pdf-wrapper">
+    <div class="appTextButtons">
+      <v-btn-group density="comfortable" variant="outlined" divided>
+        <v-btn title="Test" size="small" @click="test()">Test</v-btn>
+      </v-btn-group>
+    </div>
+    <iframe id="app-instructions-pdf" tabindex="0"
+        :title="resource.title"
+        :src="'./pdfjs-dist/web/viewer.html?file=' + encodeURIComponent(resource.url)"
+        @load="loaded"
+        frameborder="0"
+    ></iframe>
   </div>
 </template>
 
 
 <style scoped>
 
-div {
+#app-instructions-pdf-wrapper {
   height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.appTextButtons {
+  text-align: center;
+  padding-bottom: 5px;
+  height: 50px;
+}
+
+#app-instructions-pdf {
+  flex-grow: 1;
+  width: 100%;
+  height: calc(100% - 50px);
 }
 
 </style>
